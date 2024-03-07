66aac785934ebb44016dba31c3729a9bbca57fbf
JimeneVictor <162595405+JimeneVictor@users.noreply.github.com>
2024-03-07 09:21:08 -0600

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -4,7 +4,8 @@ public class calculadora {
     public static void main(String[] args) {
         // Crear un objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
-        
+        int op;
+        do{    
         // Solicitar al usuario que ingrese el primer número
         System.out.println("Ingrese el primer número:");
         double num1 = scanner.nextFloat();
@@ -21,15 +22,15 @@ public class calculadora {
         System.out.println("4. División");
         System.out.println("5. Raiz");
         System.out.println("6. Potencia");
-
+        System.out.println("7. salir");    
         // Leer la elección del usuario
-        int choice = scanner.nextInt();
+        op = scanner.nextInt();
         
         // Variable para almacenar el resultado de la operación
         double result = 0;
         
         // Realizar la operación seleccionada
-        switch(choice) {
+        switch(op) {
             case 1:
                 result = num1 + num2;
                 break;
@@ -59,12 +60,22 @@ public class calculadora {
             case 6:
                 result= Math.pow(num1,num2);
             break;
+            case 7:
+                System.out.println("bay bay");
+            break;
+
             default:
                 System.out.println("Opción no válida.");
-                return;
+        
         }
         
+        if(op != 7){
         // Mostrar el resultado de la operación
         System.out.println("El resultado es: " + result);
-    }
+        }
+            
+    }while(op != 7);
+    
+}
+    
 }
